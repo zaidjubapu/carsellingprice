e8116fa607aaad261f5e51fb5af5084af86ecf02
zaid jubapu <zaidjubapu93@gmail.com>
2020-09-01 18:05:59 +0530

    all are saved

---
+++ 1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 00:24:56 2020
+
+@author: DELL
+"""
+
+
+print("this is first spyder code")
---
+++ procfile1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 16:56:35 2020
+
+@author: DELL
+"""
+
+
+web: gunicorn app:app
